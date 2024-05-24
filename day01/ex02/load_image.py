177b4c5d9fb9fb2027d6ee369db0a0a8cb663e08
hasabir <sabirhajar048@gmail.com>
2024-05-24 20:59:57 +0100

    ex 02 begin ex 03

--- day01/ex02/load_image.py
+++ day01/ex02/load_image.py
@@ -1,16 +1,32 @@
 from PIL import Image
+import numpy
+import cv2
 
 
-img = Image.open("landscape.jpg")
-# # img.show()
-px = img.load()
-print(list(img.getpixel((0,0))))
+
+def ft_load(path: str) -> numpy.array:
+	img = Image.open(path)
+	image = cv2.imread(path)
+
+	print('The shape of image is:',image.shape)
+
+	# px = img.load()
+	# width, height = img.size
+	pixel_values = list(img.getdata())
+	pixel_values = numpy.array(pixel_values).reshape(image.shape)
+	return pixel_values
+	
+
+def main():
+	print(ft_load("landscape.jpg"))
+
+
+
+if __name__ == "__main__":
+	main()
+# print(list(img.getpixel((2,0))))
 # print(list())
 
 
 
-import cv2
 
-image = cv2.imread('landscape.jpg')
-
-print('The shape of image is:',image.shape)
